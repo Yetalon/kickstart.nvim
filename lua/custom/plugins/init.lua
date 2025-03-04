597b3213772f44b87f6c139cbac354cbf57cde70
Yetalon <Davisac7@etsu.edu>
2025-03-04 07:46:27 -0500

    settings update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,13 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
+
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'windwp/nvim-ts-autotag',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
+}
